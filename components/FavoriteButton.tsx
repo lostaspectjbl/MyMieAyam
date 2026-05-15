cd26b2604ac25863964415af911525ac0cfddb69
lostaspectjbl <lostaspectjbl@gmail.com>
2026-05-15 08:19:30 +0700

    next js error fix

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -61,6 +61,7 @@ export default function FavoriteButton({ warungId, userId }: Props) {
 
     return (
         <button
+            suppressHydrationWarning
             onClick={toggleFavorit}
             disabled={loading}
             className={`group flex items-center gap-2 px-5 py-2.5 rounded-xl font-medium transition-all duration-200 ${isFavorited
